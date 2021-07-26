333a06eb2ce9312f5fccec955deac2bc0f8f2235
xtramann1 <69823018+xtramann1@users.noreply.github.com>
2021-07-26 18:12:57 -0400

    Update SismosRest.java

--- RestSismos/src/main/java/com/ApiRest/Rest/V1/SismosRest.java
+++ RestSismos/src/main/java/com/ApiRest/Rest/V1/SismosRest.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,7 @@ public class SismosRest implements Serializable{
         @ApiResponse(code = 404, message = "No se ha encontrado la información solicitada", response = ErrorV0.class),
         @ApiResponse(code = 412, message = "Falló alguna precondición", response = ErrorV0.class)
     })
+	@CrossOrigin(origins = "http://localhost:8100")
 	@GetMapping(value = "/listar", consumes = {"*/*"}, produces = {"application/json;charset=utf-8"})
 	public ResponseEntity getAll() throws IOException, ParseException{
         final String ip = IPUtils.getClientIpAddress(httpRequest);
